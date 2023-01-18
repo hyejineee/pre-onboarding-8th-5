7b7491f35878d961a84ab582950bfc8410cb1d00
hyejineee <dev.hyejin04@gmail.com>
2023-01-18 11:11:56 +0900

    refactor : useAppSelector 리팩터링
    useSelector 사용 시 발생하는 성능 이슈를 예방하기 위한 코드 추가
    
    See also
      - https://kyounghwan01.github.io/blog/React/redux/redux-toolkit/#useselector-%E1%84%8B%E1%85%A1%E1%86%AB%E1%84%90%E1%85%B5%E1%84%91%E1%85%A2%E1%84%90%E1%85%A5%E1%86%AB

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,5 +1,12 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import {
+  shallowEqual,
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 import { AppDispatch, RootState } from './store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = <T>(
+  fn: (state: RootState) => T,
+): T => useSelector(fn, shallowEqual);
