78c41b47d77860286e2586e3917cea5b4e929179
hyejineee <dev.hyejin04@gmail.com>
2023-01-18 19:39:01 +0900

    feat : repository, http client 인터페이스 추가 및 comment 타입 정의

---
+++ src/commons/types/module.types.ts
@@ -0,0 +1,16 @@
+import CommentType from './comment.types';
+
+export interface IHttpClient {
+  get: (endPoint: string, options?: any) => Promise<any>;
+  post: (endPoint: string, payload: any, options?: any) => Promise<any>;
+  delete: (endPoint: string, options?: any) => Promise<any>;
+  put: (endPoint: string, payload: any, options?: any) => Promise<any>;
+}
+
+export interface ICommentRepository {
+  fetchComments: () => Promise<CommentType[]>;
+  fetchComment: (id: number) => Promise<CommentType>;
+  createComment: (newComment: CommentType) => void;
+  updateComment: (id: number, updated: CommentType) => void;
+  deleteComment: (id: number) => void;
+}
